e10122f77c4fe50e2a401fe50a7f8b19ae086bbe
liguojun <liguojun@bogon>
2019-10-23 15:34:47 +0800

    first_commit

---
+++ FGData/FGData.h
@@ -0,0 +1,19 @@
+//
+//  FGData.h
+//  FGData
+//
+//  Created by 肥儿古德 on 2019/10/23.
+//  Copyright © 2019 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FGData.
+FOUNDATION_EXPORT double FGDataVersionNumber;
+
+//! Project version string for FGData.
+FOUNDATION_EXPORT const unsigned char FGDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FGData/PublicHeader.h>
+
+
